f42ed8a037b6801dffcdcd33cf9eb022a03d44a3
Marcos <marcosaraya973@gmail.com>
2022-01-13 09:52:14 -0300

    asd

--- lib/src/team/repository/team_repository.dart
+++ lib/src/team/repository/team_repository.dart
@@ -1,7 +1,5 @@
 import 'package:MinkaiProject/provider/team/team_provider.dart';
 import 'package:MinkaiProject/src/team/model/team_model.dart';
-<<<<<<< HEAD
-
 class TeamRepository{
   TeamProvider _teamProvider = TeamProvider();
 
@@ -10,13 +8,3 @@ class TeamRepository{
   }
 }
 
-=======
-import 'package:rxdart/rxdart.dart';
-
-class TeamRepository {
-  TeamProvider _teamProvider = TeamProvider();
-
-  final PublishSubject<List<TeamModel>> teams =
-      PublishSubject<List<TeamModel>>();
-}
->>>>>>> d292a3000c365b779cd389f257df557562a2c2f4
